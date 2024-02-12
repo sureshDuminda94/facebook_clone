4785c5fe262d744f44cea7dad88563fe5b65be44
sureshDuminda94 <114845061+sureshDuminda94@users.noreply.github.com>
2024-02-12 01:24:43 -0600

    Updates

---
+++ precache-manifest.475c2fc213b2b75693f92856dcda8739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5901f10d36405d74eafb5a4d97c3fb",
+    "url": "/sureshDuminda94.gthub.io/facebook_clone/index.html"
+  },
+  {
+    "revision": "51cb1676da5e2cf460a6",
+    "url": "/sureshDuminda94.gthub.io/facebook_clone/static/css/main.58fee343.chunk.css"
+  },
+  {
+    "revision": "b116c5a09128c07958a0",
+    "url": "/sureshDuminda94.gthub.io/facebook_clone/static/js/2.e99413ab.chunk.js"
+  },
+  {
+    "revision": "e5a7172ac4614a7d76bf",
+    "url": "/sureshDuminda94.gthub.io/facebook_clone/static/js/3.c1a7ea53.chunk.js"
+  },
+  {
+    "revision": "51cb1676da5e2cf460a6",
+    "url": "/sureshDuminda94.gthub.io/facebook_clone/static/js/main.0248297d.chunk.js"
+  },
+  {
+    "revision": "3926541272cd92e4929f",
+    "url": "/sureshDuminda94.gthub.io/facebook_clone/static/js/runtime~main.e6e74600.js"
+  }
+]);
